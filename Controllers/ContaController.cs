82661244423973d2682c24b08de858d8a60f9d3b
rafael <rafael.francisco111@gmail.com>
2021-11-18 17:18:16 +0000

    gitignoreFix

--- Controllers/ContaController.cs
+++ Controllers/ContaController.cs
@@ -10,16 +10,19 @@ using vCardPlatform.Models;
 
 namespace vCardPlatform.Controllers
 {
-   [RoutePrefix("api/conta")]
     public class ContaController : ApiController
     {
+        string connectionString = "Data Source=(LocalDB)\\MyInstance;AttachDbFilename=C:\\Users\\rafae\\source\\repos\\vCardPlatform\\App_Data\\Database.mdf;Integrated Security=True";
+        //Co
 
-        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ProductsApp.Properties.Settings.ConnectionToDB"].ConnectionString;
+
+
         // GET: Conta/GetContaById/1
         public IHttpActionResult GetContaById(int id)
         {
             SqlConnection connection = null;
-             try
+
+            try
             {
                 connection = new SqlConnection(connectionString);
                 connection.Open();
@@ -36,11 +39,12 @@ namespace vCardPlatform.Controllers
                     pedidoAInserir.Id = (int)reader["Id"];
                     pedidoAInserir.Balance = (float)reader["Balance"];
                     pedidoAInserir.AccountOwner = (string)reader["AccountOwner"];
-                    pedidoAInserir.CreatedAt = (string)reader["CreatedAt"];
-                    pedidoAInserir.Email = (string)reader["Email"];
-                    pedidoAInserir.PhoneNumber = (string)reader["PhoneNumber"];
-                    pedidoAInserir.ConfirmationCode = (int)reader["ConfirmationCode"];
-                    
+                    string value = (string)reader["CreatedAt"];
+                    if (value == null)
+                    {
+                        pedidoAInserir.CreatedAt = DateTime.Now.Ticks+"";
+                    }
+                    pedidoAInserir.CreatedAt = value;
                 }
 
                 reader.Close();
@@ -65,7 +69,7 @@ namespace vCardPlatform.Controllers
             }
         }
 
-        [Route("")]
+        // GET: Conta/GetContaById/1
         public IHttpActionResult GetAllContas()
         {
             SqlConnection connection = null;
@@ -80,26 +84,27 @@ namespace vCardPlatform.Controllers
                 SqlDataReader reader = command.ExecuteReader();
                 LinkedList<Conta> contas = new LinkedList<Conta>(); 
 
-                
+                Conta pedidoAInserir = null;
 
                 while (reader.Read())
                 {
-                    Conta pedidoAInserir = new Conta();
+                    pedidoAInserir = new Conta();
                     pedidoAInserir.Id = (int)reader["Id"];
                     pedidoAInserir.Balance = (float)reader["Balance"];
                     pedidoAInserir.AccountOwner = (string)reader["AccountOwner"];
-                    pedidoAInserir.CreatedAt = (string)reader["CreatedAt"];
-                    pedidoAInserir.Email = (string)reader["Email"];
-                    pedidoAInserir.PhoneNumber = (string)reader["PhoneNumber"];
-                    pedidoAInserir.ConfirmationCode = (int)reader["ConfirmationCode"];
-                    contas.AddLast(pedidoAInserir);
+                    string value = (string)reader["CreatedAt"];
+                    if (value == null)
+                    {
+                        pedidoAInserir.CreatedAt = DateTime.Now.Ticks + "";
+                    }
+                    pedidoAInserir.CreatedAt = value;
                 }
 
                 reader.Close();
                 connection.Close();
-                if (contas.Count != 0)
+                if (pedidoAInserir != null)
                 {
-                    return Ok(contas);
+                    return Ok(pedidoAInserir);
                 }
                 else
                 {
@@ -117,174 +122,6 @@ namespace vCardPlatform.Controllers
             }
         }
 
-        
-        [Route("")]
-        [HttpPost]
-        public IHttpActionResult PostConta([FromBody] Conta value)
-        {
-            SqlConnection connection = null;
-            
-            try
-            {
-               
-                connection = new SqlConnection(connectionString);
-                connection.Open();
-                string cmdSQL = "INSERT INTO Contas VALUES (@z,@a,@b,@c,@d,@e,@f)";
-                SqlCommand command = new SqlCommand(cmdSQL, connection);
-                command.Parameters.AddWithValue("@z", value.Id);
-                command.Parameters.AddWithValue("@a", value.AccountOwner);
-                command.Parameters.AddWithValue("@b", value.Balance);
-                command.Parameters.AddWithValue("@c", DateTime.Now.Ticks+"");
-                command.Parameters.AddWithValue("@d", value.Email);
-                command.Parameters.AddWithValue("@e", value.ConfirmationCode);
-                command.Parameters.AddWithValue("@f", value.PhoneNumber);
-
-                int numRows = command.ExecuteNonQuery();
-
-                connection.Close();
-
-                if (numRows > 0)
-                {
-                    return Ok();
-                }
-
-                return NotFound();
-
-            }
-            catch (Exception e)
-            {
-                if (connection.State == System.Data.ConnectionState.Open)
-                {
-                    connection.Close();
-                }
-
-                return Ok(e.Message + e.StackTrace); ;
-            }
-
-        }
-
-
-        [Route("{id}")]
-        [HttpDelete]
-        public IHttpActionResult DeleteConta(int id)
-        {
-            SqlConnection connection = null;
-
-            try
-            {
-                connection = new SqlConnection(connectionString);
-                connection.Open();
-                string cmdSQL = "DELETE FROM contas WHERE Id=@IdPedido";
-                SqlCommand command = new SqlCommand(cmdSQL, connection);
-                command.Parameters.AddWithValue("@IdPedido", id);
-                int numRows = command.ExecuteNonQuery();
-
-                connection.Close();
-
-                if (numRows > 0)
-                {
-                    return Ok();
-                }
-
-                return NotFound();
-
-            }
-            catch (Exception)
-            {
-                if (connection.State == System.Data.ConnectionState.Open)
-                {
-                    connection.Close();
-                }
-
-                return NotFound();
-            }
-        }
-
-
-        [Route("{int:id}")]
-        [HttpPut]
-        public IHttpActionResult PutConta(int id,[FromBody] Conta value)
-        {
-            SqlConnection connection = null;
-
-            try
-            {
-               
-
-
-                connection = new SqlConnection(connectionString);
-                connection.Open();
-                string cmdSQL = "SELECT * FROM Contas WHERE Id=@idPedidosTable";
-                SqlCommand command = new SqlCommand(cmdSQL, connection);
-                command.Parameters.AddWithValue("@idPedidosTable", id);
-                SqlDataReader reader = command.ExecuteReader();
-
-                cmdSQL = "INSERT INTO Contas(AccountOwner,Email,ConfirmationCode,PhoneNumber) VALUES (@a,@b,@c,@d)";
-                command = new SqlCommand(cmdSQL, connection);
-                if(value.AccountOwner != null)
-                {
-                    command.Parameters.AddWithValue("@a", value.AccountOwner);
-                }
-                            
-                if(value.Email != null && IsValidEmail(value.Email))
-                {
-                    command.Parameters.AddWithValue("@b", value.Email);
-                }
-
-                if (value.ConfirmationCode> 9999 && value.ConfirmationCode<1000)
-                {
-                    command.Parameters.AddWithValue("@c", value.ConfirmationCode);
-                }
-
-                if (value.PhoneNumber.Length>8 && value.PhoneNumber.Length<14 )
-                {
-                    command.Parameters.AddWithValue("@d", value.PhoneNumber);
-                }
-                
-                
-
-                int numRows = command.ExecuteNonQuery();
-
-                connection.Close();
-
-                if (numRows > 0)
-                {
-                    return Ok();
-                }
-
-                return NotFound();
-
-            }
-            catch (Exception e)
-            {
-                if (connection.State == System.Data.ConnectionState.Open)
-                {
-                    connection.Close();
-                }
-
-                return Ok(e.Message + e.StackTrace); ;
-            }
-
-        }
-
-
-        //https://stackoverflow.com/questions/1365407/c-sharp-code-to-validate-email-address
-        bool IsValidEmail(string email)
-        {
-            if (email.Trim().EndsWith("."))
-            {
-                return false; // suggested by @TK-421
-            }
-            try
-            {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
-            }
-            catch
-            {
-                return false;
-            }
-        }
 
 
     }
--- Models/Conta.cs
+++ Models/Conta.cs
@@ -11,8 +11,7 @@ namespace vCardPlatform.Models
         public string AccountOwner { get; set; }
         public float Balance { get; set; }
         public string CreatedAt{get;set;}
-        public string Email{get;set;}
-        public int ConfirmationCode{get;set;}
-        public string PhoneNumber{get;set;}
+
+
     }
 }
